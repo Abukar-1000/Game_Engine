d5b448f12d8be7caefc5db26ea9767dd16f4e559
Abukar-1000 <abukardjama1@gmail.com>
2023-06-09 02:26:49 -0400

    start 2

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config(); // read .env files
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set public folder as root
+app.use(express.static('public'));
+
+// Allow front-end access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+
+// entry point 
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
+})
+// Listen for HTTP requests on port 3000
+app.listen(port, () => {
+  console.log('listening on %d', port);
+});
