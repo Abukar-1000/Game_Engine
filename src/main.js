53b5d40105e9adc1d3031e0efbd4b98242f1397a
Abukar-1000 <abukardjama1@gmail.com>
2023-06-09 02:07:10 -0400

    start

---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+
+function main(e) {
+    document.querySelector("body").innerHTML = "<h1>Firing with V Cylinders!!!</h1>";
+}
+
+// runs app when document has loaded 
+document.addEventListener("DOMContentLoaded", main, false);
